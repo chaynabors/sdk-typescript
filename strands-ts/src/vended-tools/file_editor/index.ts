fef0ed8c310882a278364ea0b18f889c00613b86
Chay Nabors <nabochay@amazon.com>
2026-04-20 16:53:16 -0400

    [mono] Remove strands-ts for subtree merge into sdk-typescript

--- strands-ts/src/vended-tools/file_editor/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * File editor tool for programmatic filesystem interaction.
- */
-
-export { fileEditor } from './file-editor.js'
-export type { FileEditorInput, FileEditorOptions, IFileReader } from './types.js'
